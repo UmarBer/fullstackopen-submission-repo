8b858f9fa1b95eb072fb1832d063fe02493d86a7
Umar Bermejo <umarber94@gmail.com>
2023-06-20 18:37:14 +0200

    Exercise 1.1

---
+++ 1.a Intro to React/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: '24' },
+    { name: 'Maya', age: '25' }
+  ];
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={friends[0].name} age={friends[0].age} />
+      <Hello name={friends[1].name} age={friends[1].age} />
+    </>
+  );
+};
+
+export default App;
