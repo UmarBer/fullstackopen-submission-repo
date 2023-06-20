85041a820c36b001943492ff3111cef050ab54d3
Umar Bermejo <umarber94@gmail.com>
2023-06-20 19:03:19 +0200

    Exercise 1.2

--- exercises/courseinfo/src/App.jsx
+++ exercises/courseinfo/src/App.jsx
@@ -1,9 +1,9 @@
 const App = () => {
   const course = 'Half Stack application development';
   const parts = [
-    { part1: 'Fundamentals of React', exercises: 10 },
-    { part2: 'Using props to pass data', exercises: 7 },
-    { part3: 'State of a component', exercises: 14 }
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 }
   ];
 
   const Header = (props) => {
@@ -14,17 +14,24 @@ const App = () => {
     );
   };
 
+  const Part = (props) => {
+    console.log(props);
+    return (
+      <>
+        <h3>{props.part}</h3>
+        <p>Number of exercises: {props.exercises}</p>
+      </>
+    );
+  };
+
   const Content = () => {
     console.log(parts);
     return (
       <>
         <h2>Content:</h2>
-        <h3>{parts[0].part1}</h3>
-        <p>Number of exercises: {parts[0].exercises}</p>
-        <h3>{parts[1].part2}</h3>
-        <p>Number of exercises: {parts[1].exercises}</p>
-        <h3>{parts[2].part3}</h3>
-        <p>Number of exercises: {parts[2].exercises}</p>
+        <Part part={parts[0].part} exercises={parts[0].exercises} />
+        <Part part={parts[1].part} exercises={parts[1].exercises} />
+        <Part part={parts[2].part} exercises={parts[2].exercises} />
       </>
     );
   };
